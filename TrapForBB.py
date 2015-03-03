c69a7e5855cf1cc4a058051d356cb0d181c2a9d4
Long <longzhiw@ueca86bc3202154d1cd89.ant.amazon.com>
2015-03-03 16:23:43 +0800

    1. Drop title which characters less then 8; 2. Added requirements;

--- TrapForBB.py
+++ TrapForBB.py
@@ -1,29 +1,32 @@
+#!/usr/bin/env python
 #--* coding=utf-8 *--
 import urllib2
 from bs4 import BeautifulSoup as bs
-import base64 
+import base64
 import subprocess
 import re
 import time
 from logger import logger
 
-# Define 
+# Define
 URLDICT = {
-    u"南宁市科技局":"http://www.nnst.gov.cn/",
-    u"南宁市工信委":"http://219.159.80.227/index.htm",
-    u"南宁市发改委":"http://fgw.nanning.gov.cn/",
-    u"南宁市农业局":"http://www.nnny.gov.cn/",
-    u"南宁市环保局":"http://www.nnhb.gov.cn/",
-    u"南宁市商务局":"http://sw.nanning.gov.cn/",
-    u"广西工信委":"http://www.gxgxw.gov.cn/",
-    u"广西科技厅":"http://www.gxst.gov.cn/",
-    u"广西发改委":"http://www.gxdrc.gov.cn/",
-    u"广西农业信息网":"http://www.gxny.gov.cn/",
-    u"广西商务厅":"http://www.gxswt.gov.cn/",
-    u"南宁市中小企业信息网":"http://www.smenn.gov.cn/",
-    u"广西中小企业信息网":"http://www.smegx.gov.cn/",
-    }
-KEYWORD = [u'申报',u'项目',u'通知',u'验收',u'立项',u'资金',u'课题']
+    u"南宁市科技局": "http://www.nnst.gov.cn/",
+    u"南宁市工信委": "http://219.159.80.227/index.htm",
+    u"南宁市发改委": "http://fgw.nanning.gov.cn/",
+    u"南宁市农业局": "http://www.nnny.gov.cn/",
+    u"南宁市环保局": "http://www.nnhb.gov.cn/",
+    u"南宁市商务局": "http://sw.nanning.gov.cn/",
+    u"广西工信委": "http://www.gxgxw.gov.cn/",
+    u"广西科技厅": "http://www.gxst.gov.cn/",
+    u"广西发改委": "http://www.gxdrc.gov.cn/",
+    u"广西农业信息网": "http://www.gxny.gov.cn/",
+    u"广西商务厅": "http://www.gxswt.gov.cn/",
+    u"南宁市中小企业信息网": "http://www.smenn.gov.cn/",
+    u"广西中小企业信息网": "http://www.smegx.gov.cn/",
+}
+KEYWORD = [u'申报', u'项目', u'通知', u'验收', u'立项', u'资金', u'课题']
+AVOIDWORD = [u'通知公告', u'在线申报', u'招商项目', u'项目申报指南', u'项目申报',
+             u'农村经济项目', u'招商项目']
 
 
 def trapAllLinks(URLDICT):
@@ -41,18 +44,23 @@ def trapAllLinks(URLDICT):
             pass
     return result_list
 
+
 def parseURL(website, a_soup):
     result_dict = {}
     for key_word in KEYWORD:
         for link in a_soup:
-            if key_word in link.text:
-                result_dict[link.text.strip()] = sillyURLReformat(website, link['href'])
+            # Title less than 8 characters will be drop
+            if key_word in link.text and len(link.text.strip()) > 8:
+                result_dict[link.text.strip()] = sillyURLReformat(
+                    website, link['href'])
     return result_dict
+
 
 def sillyURLReformat(website, url):
     if url.startswith("./"):
         url = URLDICT[website] + url.replace("./", "")
     return url
+
 
 def main():
     start = time.time()
@@ -64,17 +72,18 @@ def main():
         logger.debug("Parsing %s..." % website)
         title_and_link = parseURL(website, a_soup)
         result_dict[website] = title_and_link
-        logger.debug("Parsed %s, Matched %d links." % (website, len(title_and_link)))
+        logger.debug("Parsed %s, Matched %d links." %
+                     (website, len(title_and_link)))
 
     # Show result
     print '=' * 80
     print 'Result'
     print '=' * 80
     for website, title_and_link in result_dict.items():
-        print 'Result of %s : %s' % (website, URLDICT[website])
+        print 'Result of %s : %s' % (website.encode("utf-8"), URLDICT[website])
         print '-' * 40
         for title, link in title_and_link.items():
-            print '- %s' % title
+            print '- %s' % title.encode("utf-8")
             print '- %s' % link
             print ''
         print '-' * 40
@@ -88,12 +97,14 @@ def main():
 
     exportToHTML(result_dict)
 
+
 def exportToHTML(result_dict):
     table_container = ""
     for website, title_and_link in result_dict.items():
         website_line = "<tr><td><H2>" + website + "</H2></td></tr>"
-        for title,link in title_and_link.items():
-            link_line = "<tr><td>" + "<a href=\"%s\">%s</a>" % (link, title) + "</td></tr>"
+        for title, link in title_and_link.items():
+            link_line = "<tr><td>" + \
+                "<a href=\"%s\">%s</a>" % (link, title) + "</td></tr>"
             website_line += link_line
         website_line += "<tr><td></td></tr>"
         table_container += website_line
@@ -119,7 +130,6 @@ def exportToHTML(result_dict):
         file_time.tm_min,)
     with open('./' + file_name, 'w') as f:
         f.write(html_container.encode("utf-8"))
-
 
 
 if __name__ == '__main__':
